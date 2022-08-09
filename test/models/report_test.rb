983c71cef06642ea8f8802b31d51bd9262bac37b
horiyu <horikoshi@pepabo.com>
2022-08-09 11:41:19 +0900

    [refactor] クラス名の省略と、何度も呼ばれる場所をsetupに分割

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -2,9 +2,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  def setup
+    @user = create(:user)
+  end
+
   test 'is_editable should return true if user is target user' do
-    user = FactoryBot.create(:user)
-    report = Report.new(user_id: user.id, title: 'test title', content: 'test content')
-    report.editable?(user)
+    report = Report.new(user_id: @user.id, title: 'test title', content: 'test content')
+    report.editable?(@user)
   end
 end
