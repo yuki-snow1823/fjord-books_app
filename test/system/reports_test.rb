6c0369b4746770927050e234d46712036ea6edc7
horiyu <horikoshi@pepabo.com>
2022-08-17 16:45:25 +0900

    [fix] システムに依存しないように画面上の確認でテストを実行

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -17,7 +17,7 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in('タイトル', with: 'test title')
     fill_in('内容', with: 'test content')
     click_on('登録する')
-    assert Report.find(1)
+    assert_equal true, page.has_text?('test title')
   end
 
   test 'update report' do
@@ -25,18 +25,16 @@ class ReportsTest < ApplicationSystemTestCase
     visit edit_report_path(1)
     fill_in('タイトル', with: 'changed title')
     click_on('更新する')
-    assert_text 'changed title'
-    # find('p > strong:first-child')
+    assert_equal true, page.has_text?('changed title')
   end
 
   test 'delete report' do
-    create(:report, user_id: @user.id)
-    report_count = Report.all.count
+    create(:report, user_id: @user.id, title: 'test title')
     visit reports_path
     accept_confirm do
       click_on '削除'
     end
     visit reports_path
-    assert_equal report_count - 1, Report.all.count
+    assert_equal true, page.has_no_text?('test title')
   end
 end
